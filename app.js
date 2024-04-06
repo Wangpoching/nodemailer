562343dc8d7e7240b16f553a278aab5b8fa14743
Peter <peter@cacdi.com>
2024-04-06 10:13:33 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+var createError = require('http-errors');
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var path = require('path');
+var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth');
+var emailRouter = require('./routes/email');
+var session = require('express-session');
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({     // Set up session middleware
+  secret: 'woof', // Change this to your own secret key
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/users', usersRouter);
+app.use('/auth', authRouter);
+app.use('/email', emailRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
